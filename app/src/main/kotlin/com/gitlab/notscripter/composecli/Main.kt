f2438f71f7664fbda81d1b21559db3863920990b
MRINMOY MONDAL <mm2922894@gmail.com>
2025-07-31 02:53:13 +0530

    working on launcher subcommand

--- app/src/main/kotlin/com/gitlab/notscripter/composecli/Main.kt
+++ app/src/main/kotlin/com/gitlab/notscripter/composecli/Main.kt
@@ -14,11 +14,13 @@ import com.github.ajalt.mordant.rendering.TextColors.*
 import com.github.ajalt.mordant.rendering.TextStyles.*
 import com.github.ajalt.mordant.terminal.Terminal
 import java.io.File
+import java.io.FileWriter
 import java.io.IOException
 import java.net.URLDecoder
 import java.nio.file.Files
 import java.util.regex.Pattern
 import kotlin.collections.emptyList
+import kotlin.io.deleteRecursively
 
 val t = Terminal()
 
@@ -205,69 +207,104 @@ private fun resizeImage(image: File, size: String, output: File) {
 class Launcher : SuspendingCliktCommand() {
     override fun help(context: Context) = "Modify the launcher icon of the app"
 
-    private val foreground by option("-f", "--foreground")
-    private val background by option("-b", "--background")
+    private val foreground by option("-f", "--foreground").required()
+    private val background by option("-b", "--background").required()
+
+    private val foregroundImage = File(foreground)
+    private var backgroundImage = File(background)
 
     private class Mipmap(name: String, imageSize: Int, tempDir: File) {
         val imageSize: String = "${imageSize}x${imageSize}"
         val dir: File = File(tempDir, "mipmap-${name}")
     }
 
+    private val tempDir = Files.createTempDirectory("compose-launcher").toFile()
+    private val tempResDir = Files.createTempDirectory("compose-launcher-res").toFile()
+
+    private val resDir = File("./app/src/main/res")
+
+    private val mipmaps =
+        listOf<Mipmap>(
+            Mipmap("xxxhdpi", 432, tempResDir),
+            Mipmap("xxhdpi", 324, tempResDir),
+            Mipmap("xhdpi", 216, tempResDir),
+            Mipmap("hdpi", 162, tempResDir),
+            Mipmap("mdpi", 108, tempResDir),
+        )
+
     override suspend fun run() {
-        val resDir = File("./app/src/main/res")
+        if (!foregroundImage.exists()) error("cant find foreground image")
+        if (!backgroundImage.exists()) {
+            backgroundImage = File(tempDir, "background.png")
+            if (isValidHexaCode(background)) {
+                sh("magick -size 1024x1024 xc:${background} ${backgroundImage}")
+            } else {
+                sh("magick -size 1024x1024 xc:#ffffff ${backgroundImage}")
+            }
+        }
+
         if (!resDir.exists()) error("res dir dont exists")
-
-        val tempResDir = Files.createTempDirectory("launcerRes").toFile()
-
-        // val xxxhdpiDir = File(tempResDir, "mipmap-xxxhdpi")
-        // val xxhdpiDir = File(tempResDir, "mipmap-xxhdpi")
-        // val xhdpiDir = File(tempResDir, "mipmap-xhdpi")
-        // val hdpiDir = File(tempResDir, "mipmap-hdpi")
-        // val mdpiDir = File(tempResDir, "mipmap-mdpi")
-        // val anydpiV26Dir = File(tempResDir, "mipmap-anydpi-v26")
-        //
-        // xxxhdpiDir.mkdir()
-        // xxhdpiDir.mkdir()
-        // xhdpiDir.mkdir()
-        // hdpiDir.mkdir()
-        // mdpiDir.mkdir()
-        // anydpiV26Dir.mkdir()
-
-        val mipmaps =
-            listOf<Mipmap>(
-                Mipmap("xxxhdpi", 432, tempResDir),
-                Mipmap("xxhdpi", 324, tempResDir),
-                Mipmap("xhdpi", 216, tempResDir),
-                Mipmap("hdpi", 162, tempResDir),
-                Mipmap("mdpi", 108, tempResDir),
-            )
         mipmaps.forEach { it.dir.mkdir() }
 
-        if (foreground != null) {
-            mipmaps.forEach {
-                sh(
-                    "magick ${foreground} -resize ${it.imageSize} ${File(it.dir, "ic_launcher_foreground.png")}"
-                )
-            }
+        val playStore = Mipmap("play_store", 512, tempResDir.parentFile)
+        val anydpiIcLauncher = File(tempResDir, "mipmap-anydpi-v26/ic_launcher.xml")
+
+        // if (!anydpi.exists()) anydpiDir.mkdir()
+        if (!anydpiIcLauncher.exists()) {
+            anydpiIcLauncher.createNewFile()
+            val icLauncher = FileWriter(anydpiIcLauncher)
+            icLauncher.write(
+                """
+                <?xml version="1.0" encoding="utf-8"?>
+                <adaptive-icon xmlns:android="http://schemas.android.com/apk/res/android">
+                  <background android:drawable="@mipmap/ic_launcher_background"/>
+                  <foreground android:drawable="@mipmap/ic_launcher_foreground"/>
+                  <monochrome android:drawable="@mipmap/ic_launcher_monochrome"/>
+                </adaptive-icon>
+                """
+            )
         }
 
-        when (background != null) {
-            isValidHexaCode(background.toString()) -> {
-                mipmaps.forEach {
-                    sh(
-                        "magick -size ${it.imageSize} xc:${background} ${File(it.dir, "ic_launcher_background.png")}"
-                    )
-                }
-            }
-            File(background).exists() -> {
-                mipmaps.forEach {
-                    sh(
-                        "magick ${background} -resize ${it.imageSize} ${File(it.dir, "ic_launcher_background.png")}"
-                    )
-                }
-            }
-            else -> error("please provide valid background")
+        if (foregroundImage.exists() && backgroundImage.exists()) {
+            val play_store_foreground = File(tempDir, "play_store_foreground.png")
+            val play_store_background = File(tempDir, "play_store_background.png")
+            sh("magick -resize ${playStore.imageSize} ${foregroundImage} ${play_store_foreground}")
+            sh("magick -resize ${playStore.imageSize} ${backgroundImage} ${play_store_background}")
+            sh(
+                "magick ${play_store_background} ${play_store_foreground} -gravity center -composite ${File(playStore.dir, "play_store_512.png")}"
+            )
         }
+
+        mipmaps.forEach {
+            sh(
+                "magick ${foreground} -resize ${it.imageSize} ${File(it.dir, "ic_launcher_foreground.png")}"
+            )
+        }
+        mipmaps.forEach {
+            sh(
+                "magick ${background} -resize ${it.imageSize} ${File(it.dir, "ic_launcher_background.png")}"
+            )
+        }
+
+        // when (background != null) {
+        //     isBackgroundColor -> {
+        //         mipmaps.forEach {
+        //             sh(
+        //                 "magick -size ${it.imageSize} xc:${backgroundColor} ${File(it.dir,
+        // "ic_launcher_background.png")}"
+        //             )
+        //         }
+        //     }
+        //     File(background).exists() -> {
+        //         mipmaps.forEach {
+        //             sh(
+        //                 "magick ${background} -resize ${it.imageSize} ${File(it.dir,
+        // "ic_launcher_background.png")}"
+        //             )
+        //         }
+        //     }
+        //     else -> error("please provide valid background")
+        // }
 
         t.println(tempResDir)
     }
