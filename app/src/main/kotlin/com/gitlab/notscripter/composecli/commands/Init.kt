449c1d772a9c406687748d89976fbd27e15e3e74
MRINMOY MONDAL <mm2922894@gmail.com>
2025-09-18 20:44:26 +0530

    up

--- app/src/main/kotlin/com/gitlab/notscripter/composecli/commands/Init.kt
+++ app/src/main/kotlin/com/gitlab/notscripter/composecli/commands/Init.kt
@@ -6,6 +6,8 @@ import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.arguments.optional
 import com.github.ajalt.clikt.parameters.options.help
 import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.optionalValue
+import com.github.ajalt.clikt.parameters.options.prompt
 import com.github.ajalt.mordant.rendering.TextColors.*
 import com.github.ajalt.mordant.rendering.TextStyles.*
 import com.gitlab.notscripter.composecli.compose.getTemplateDir
@@ -18,13 +20,16 @@ class Init : SuspendingCliktCommand() {
     override fun help(context: Context) =
         "  Kickstart a new Jetpack Compose app from your terminal."
 
-    private val projectNameOpt by option("-n", "--name").help("App name (e.g., MyComposeApp)")
+    private val projectNameOpt by
+        option("-n", "--name").prompt().help("App name (e.g., MyComposeApp)").optionalValue()
     private val projectIdOpt by
-        option("-p", "--package").help("Package name (e.g., com.example.app)")
+        option("-p", "--package").prompt().help("Package name (e.g., com.example.app)")
     private val projectPathOpt by
         option("-l", "--location").help("Where to create the app (default: current directory)")
     private val templateNameOpt by
-        option("-t", "--template").help("Template to use (use `compose list-templates` to see all)")
+        option("-t", "--template")
+            .prompt()
+            .help("Template to use (use `compose list-templates` to see all)")
 
     private val projectNameArg by argument("name").optional()
     private val projectIdArg by argument("package").optional()
@@ -32,21 +37,28 @@ class Init : SuspendingCliktCommand() {
     private val templateNameArg by argument("template").optional()
 
     override suspend fun run() {
-        val projectName = projectNameOpt ?: projectNameArg
-        val projectId = projectIdOpt ?: projectIdArg
+        val projectName: String? = projectNameOpt ?: projectNameArg
+        val projectId: String? = projectIdOpt ?: projectIdArg
         val projectPath = projectPathOpt ?: projectPathArg
-        val templateName = templateNameOpt ?: templateNameArg
+        val templateName: String? = templateNameOpt ?: templateNameArg
 
         if (projectName == null) {
-            error("project name is reqruied")
+            t.println(red("Project name is reqruied"))
+            return
         } else if (projectId == null) {
-            error("project id is reqruied")
+            t.println(red("Project id is reqruied"))
+            return
+        } else if (templateName == null) {
+            t.println(red("Tempate name is reqruied"))
+            return
         }
 
         val templateDir = getTemplateDir(templateName ?: "EmptyActivity")
-        if (templateDir == null) return
 
-        if (!templateDir.exists()) t.println(red("Template not found"))
+        if (templateDir == null || !templateDir.exists()) {
+            t.println(red("Template not found"))
+            return
+        }
 
         val tempDir = Files.createTempDirectory("compose-cli-template").toFile()
         templateDir.copyRecursively(tempDir, overwrite = true)
@@ -55,7 +67,10 @@ class Init : SuspendingCliktCommand() {
         if (!updateTemplateOutput) return
 
         val destination = File(projectPath ?: projectName)
-        if (destination.exists()) error("Directory already exists")
+        if (destination.exists()) {
+            t.println(red("Directory already exists"))
+            return
+        }
 
         tempDir.copyRecursively(destination, overwrite = true)
         t.println(green("✔️ Project '${projectName}' created"))
